e692e9319fd7d13b75a66dc8c13320e9058880a9
Iskandar Fani <isk.fan@yandex.ru>
2026-04-18 21:17:59 +0300

    index html add

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello from LOCAL PC 🚀');
-});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
