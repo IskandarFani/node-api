340813280f9594fc24098285b18e411f86518f2b
Iskandar Fani <isk.fan@yandex.ru>
2026-04-18 20:41:42 +0300

    update response

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node 🚀');
+  res.send('Hello from LOCAL PC 🚀');
 });
 
 app.listen(3000, () => {
